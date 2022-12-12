d692d85188131f88c517ac86519ae788a5588870
yadukrishnan4998 <114427970+yadukrishnan4998@users.noreply.github.com>
2022-12-12 23:36:28 +0530

    Add files via upload

---
+++ task-08/index.js
@@ -0,0 +1,36 @@
+
+
+
+let playcrash=function()
+{
+    document.getElementById("crs").play()
+}
+let playkickbass=function()
+{
+    document.getElementById("kb").play()
+}
+let playsnare=function()
+{
+    document.getElementById("sn").play()
+}
+let playtom1=function()
+{
+    document.getElementById("tom1").play()
+}
+let playtom2=function()
+{
+    document.getElementById("tom2").play()
+}
+let playtom3=function()
+{
+    document.getElementById("tom3").play()
+}
+let playtom4=function()
+{
+    document.getElementById("tom4").play()
+}
+
+
+
+
+
