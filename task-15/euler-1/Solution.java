8e763573ac124e6dfe48c67fa504224a441eb922
yadukrishnan4998 <114427970+yadukrishnan4998@users.noreply.github.com>
2022-12-14 07:16:33 +0530

    Add files via upload

---
+++ task-15/euler-1/Solution.java
@@ -0,0 +1,36 @@
+package amfoss_java;
+
+import java.util.*;
+class Solution
+{
+	public static void main(String args [])
+	{
+		Scanner sc = new Scanner (System.in);
+		int n=sc.nextInt();
+		int a[]= new int[n];
+		int sum=0;
+		int b[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int j=1;
+		for(int i=0;i<n;i++)
+		{
+		    while(j<a[i])
+		    {
+				if(j%5==0 || j%3==0)
+					sum=sum+j;
+				j++;
+		    }
+			b[i]=sum;
+			sum=0;
+			j=0;
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(b[i]);
+		}
+		
+	}
+}
